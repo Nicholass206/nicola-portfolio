43552c27fa85f83bb42812c54cc7c6531a05a186
Nicolas Fadoul <nicolafadoul@gmail.com>
2025-09-16 11:26:52 +0300

    fix mobile resume button

--- src/components/Navbar/MobileNavbar/MobileNavbar.jsx
+++ src/components/Navbar/MobileNavbar/MobileNavbar.jsx
@@ -4,8 +4,26 @@ import "./MobileNavbar.css";
 
 const MobileNavbar = ({ isOpen, toggleMenu, activeLink, setActiveLink }) => {
   const handleLinkClick = (link) => {
-    setActiveLink(link); // Set the active link
-    toggleMenu(); // Close the mobile menu
+    setActiveLink(link); 
+    toggleMenu(); 
+  };
+  
+  const downloadButton = () => {
+    try {
+      const link = document.createElement("a");
+      link.href =
+        "https://nicholass206.github.io/nicola-portfolio/images/Nicola-Fadoul-Resume.pdf";
+      link.download = "Nicola-Fadoul-Resume.pdf";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.error("Error downloading the file:", error);
+      window.open(
+        "https://nicholass206.github.io/nicola-portfolio/images/Nicola-Fadoul-Resume.pdf",
+        "_blank"
+      );
+    }
   };
 
   return (
@@ -76,7 +94,7 @@ const MobileNavbar = ({ isOpen, toggleMenu, activeLink, setActiveLink }) => {
             </Link>
           </li>
         </ul>
-        <button className="contact-btn" aria-label="Hire Me" onClick={() => {}}>
+        <button className="contact-btn" onClick={downloadButton}>
           Resume
         </button>
       </div>
